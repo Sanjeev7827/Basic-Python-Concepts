740148eefdf8c2a1831e5830ddd0f32705042372
Sanjeev Kumar <www.sanjeevkumar27194@gmail.com>
2025-08-16 21:31:31 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+def add(num1,num2):
+    return num1+num2
+
+def sub(num1,num2):
+    return num1-num2
+
+def mul(num1,num2):
+    return num1*num2
+
+def div(num1,num2):
+    return num1/num2
+
+print("addition:",add(num1,num2))
+print("subtraction:",sub(num1,num2))
+print("multiplication:",mul(num1,num2))
+print("division:",div(num1,num2))
---
+++ task2.py
@@ -0,0 +1,7 @@
+first_name=input("Enter first Name:")
+last_name=input("Enter last Name:")
+
+def greet(first_name,last_name):
+    return f"Hello, {first_name} {last_name}! welcome to the Python program"
+
+print(greet(first_name,last_name))
