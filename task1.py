88d013e68aea850882f017a2699920dfac42c9c2
Sanjeev Kumar <www.sanjeevkumar27194@gmail.com>
2025-08-19 15:40:53 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,19 +1,5 @@
-num1=int(input("Enter first number:"))
-num2=int(input("Enter second number:"))
-
-def add(num1,num2):
-    return num1+num2
-
-def sub(num1,num2):
-    return num1-num2
-
-def mul(num1,num2):
-    return num1*num2
-
-def div(num1,num2):
-    return num1/num2
-
-print("addition:",add(num1,num2))
-print("subtraction:",sub(num1,num2))
-print("multiplication:",mul(num1,num2))
-print("division:",div(num1,num2))
+num=int(input("Enter num: "))
+if num%2==0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
